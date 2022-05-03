551b4a714a7c14816aa0a7ce63d0390e3ab73991
Adarsh Raj <adarshraj2721@gmail.com>
2022-05-03 23:51:04 +0530

    Assigment-2

---
+++ block-BNaaea/E-commerce/routes/users.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../modals/user')
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  User.find({},(err ,users) =>{
+    console.log(users)
+    res.render('users' ,{users});
+  })
+});
+
+
+router.get('/register' ,(req ,res ,next) => {
+  res.render('register')
+
+})
+
+router.post('/register' ,(req ,res ,next) => {
+  //console.log(req.body)
+  User.create(req.body ,(err ,user) => {
+    console.log(err ,user)
+    res.redirect('/users/login')
+  })
+
+})
+router.get('/login' ,(req ,res ,next) => {
+  res.render('login')
+  
+})
+
+router.post('/login', function(req, res, next) {
+  var {email ,password} = req.body;
+  if(! email ||! password) {
+    // req.flash('error' ,'email/password are required')
+   return res.redirect('/users/login')
+  }
+
+if(password.length < 4){
+ // req.flash('errorPassword' ,'password less than four')
+  return res.redirect('/users/login')
+}
+  User.findOne({email} ,(err ,user) => {
+    if(err) return next(err)
+      console.log(user, "login user")
+    // no user
+    if(! user) {
+      return res.redirect('/users/login');
+    }
+
+    // compare
+    user.verifypassword(password ,(err ,result) => {
+      if(err) return next(err);
+      console.log(err ,result)
+      if(! result) {
+       // req.flash('wrongPassword' ,'password is worng')
+         return res.redirect('/users/login')
+      }
+
+// persist login user info
+      req.session.userID = user.id
+      if(user.isAdmin === true) {
+       return res.redirect('/product/new')
+      }  
+      if(user.isAdmin === false) {
+        return res.redirect('/product')
+      }
+
+    })
+  })
+
+});
+
+
+
+
+
+//block the user
+router.get('/:id/block',(req ,res) => {
+  var userId = req.params.id;
+  User.findByIdAndUpdate(userId ,{$set :{isBlocked : true}} ,(err ,blockUser) => {
+    
+  })
+})
+
+//unblock user
+router.get('/:id/Unblock' ,(req ,res) => {
+  var userId = req.params.id;
+  User.findByIdAndUpdate(userId ,{$set :{isBlocked :false}} ,(err ,unblockUser) => {
+  
+  })
+})
+
+//logout
+
+router.get('/logout' , (req ,res) => {
+  req.session.destroy();
+  res.clearCookie('Connect.sid');
+  res.redirect('/users/login');
+
+})
+module.exports = router;
