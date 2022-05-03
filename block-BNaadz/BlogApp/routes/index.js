13707681d198e3aa18c034830a6b8218f6e6925f
Adarsh Raj <adarshraj2721@gmail.com>
2022-05-03 12:06:14 +0530

    Assig-1

---
+++ block-BNaadz/BlogApp/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var auth = require('../middlewares/auth');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/protected',auth.loggedInUser,(req,res)=>{
+  res.send('Protected Resources');
+})
+
+
+module.exports = router;
